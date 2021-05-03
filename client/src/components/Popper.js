97fcdb9d87aaa2a491ce5c01007bd517303ba5f7
villagef <filwyd123@gmail.com>
2021-05-03 11:18:19 +0200

    handle popper

--- client/src/components/Popper.js
+++ client/src/components/Popper.js
@@ -1,9 +1,9 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Popper from "@material-ui/core/Popper";
 import Typography from "@material-ui/core/Typography";
 import Fade from "@material-ui/core/Fade";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,9 +19,22 @@ const useStyles = makeStyles((theme) => ({
 
 export default function AchievementPopper() {
   const classes = useStyles();
-  const cookies = useSelector(state => state.cookies);
   const [open, setOpen] = useState(false);
+  const cookies = useSelector((state) => state.cookies);
+  const breakpoint = useSelector((state) => state.breakpoint);
 
+  const handlePopper = (value) => {
+    if (value === breakpoint) {
+      setOpen(true);
+      setTimeout(() => {
+        setOpen(false);
+      }, 3000);
+    }
+  };
+
+  useEffect(() => {
+    handlePopper(cookies);
+  }, [cookies]);
 
   return (
     <Popper className={classes.root} open={open} placement="top" transition>
